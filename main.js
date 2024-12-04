9d64192415b74975b20f1117d32d0fea4f97e2bd
BeardedGrinn <mitchell.t.grinnell@hotmail.co.uk>
2024-12-04 09:50:13 +0000

    Completed task 1
    Made several arrays that work.

--- main.js
+++ main.js
@@ -3,3 +3,72 @@ const favouriteFoods = ["Cheese", "Chocolate", "Olives", "Anchovies"];
 console.log (favouriteFoods)
 // Shows array of favoriteFoods within the console log.
 
+console.log (favouriteFoods[1])
+// Test to select a specific part of the array.
+
+const favoriteSongs = [
+    "Tiny Dancer",
+    "Running Up That Hill",
+    "Don't Stop Moving",
+    "Tragedy",
+    "Crazy Horses",
+];
+
+console.log (favoriteSongs)
+
+const pennyDoublingPyramidScheme = [
+    "1",
+    "2",
+    "4",
+    "8",
+    "16",
+    "32",
+    "64",    
+]
+
+console.log (pennyDoublingPyramidScheme)
+
+const friends = [
+    {
+        name: "Chris",
+        specialSkill: "Changing Lives",
+    },
+    {
+        name: "Lizzie",
+        specialSkill: "Being Friendly",
+    },
+    {
+        name: "Ben",
+        specialSkill: "Rapping",
+    },
+    {
+        name: "Tao",
+        specialSkill: "Hand Raising"
+    },
+    {
+        name: "Liz",
+        specialSkill: "Crochet",
+    },
+    {
+        name: "Patrick",
+        specialSkill: "Call of Duty",
+    },
+    {
+        name: "Tim",
+        specialSkill: "Analogies",
+    },
+    {
+        name: "James",
+        specialSkill: "Friendship",
+    },
+    {
+        name: "Joseph",
+        specialSkill: "Being Positive",
+    },
+    {
+        name: "Max",
+        specialSkill: "Papier-mâché",
+    }
+]
+
+console.log (friends)
